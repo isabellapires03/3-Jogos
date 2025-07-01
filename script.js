bf63345e3c2d125bb56d05886998defcc838d1d1
Isabella Pires da Silva <163479024+isabellapires03@users.noreply.github.com>
2025-07-01 20:22:41 -0300

    f

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const min = 0;
+const max = 9999;
+let chute;
+let soma;
+let numero;
+let nome = prompt("Digite o seu nome");
+console.log(nome);
+document.querySelector('#header').innerHTML = 'Olá, ' + nome + '!'
+
+
+
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function novoNumero(numero){
+  numero = getRandomIntInclusive(min, max);
+  return numero;
+}
+
+function exceptionNumeroInvalido(chute){
+  if (chute <0 || chute >9999){
+    prompt("O seu chute é um número inválido!");
+  }
+}
+
+function somaNumeros(chute, numero){
+  soma = chute + numero;
+  return soma;
+}
+
 
